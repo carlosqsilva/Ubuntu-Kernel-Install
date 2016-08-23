e20d9f31a91c2d3605caf0adfa83f02fb9b8b991
carlosqsilva <carlosqsilva@outlook.com>
2016-08-23 16:32:01 -0300

    Initial commit

--- app.py
+++ app.py
@@ -7,62 +7,77 @@ from gi.repository import Gtk
 from subprocess import Popen
 from regulaexp import available_versions, get_filename
 
-class app(object):
-    def __init__(self):
+
+class app(Gtk.Window):
+
+    _url = "http://kernel.ubuntu.com/~kernel-ppa/mainline/"
+
+    def __init__(self, *args):
+        super(app, self).__init__(*args)
+        self.set_position(Gtk.WindowPosition.CENTER)
+        self.set_title("Ubuntu Kernel Install")
+        self.set_default_size(300, 400)
+        self.connect("delete-event", Gtk.main_quit)
 
         uname = os.uname()
         self.arch = 'amd64'
         if uname.machine != 'x86_64':
             self.arch = 'i386'
 
-        self._url = "http://kernel.ubuntu.com/~kernel-ppa/mainline/"
         self.linux_versions = available_versions(self._url)
         self.linux_versions.reverse()
 
-        builder = Gtk.Builder()
-        builder.add_from_file("gui.glade")
+        vbox = Gtk.VBox(spacing=6)
+        # system_info = Gtk.Label(
+        #     "YOUR KERNEL\tRelease: {}\nMachine: {}".format(uname.release, self.arch))
+        # vbox.pack_start(system_info, False, False, 0)
 
-        self.info = builder.get_object("info")
-        self.list = builder.get_object("list_versions")
-        self.what = builder.get_object("combobox")
-        self.window = builder.get_object("window1")
+        hbox = Gtk.HBox(spacing=5)
+        info_label = Gtk.Label("Choose the Type of Kernel: ")
 
         types = Gtk.ListStore(str)
         types.append(['Generic'])
         types.append(['Low Latency'])
-        self.what.set_model(types)
-        self.what.set_active(0)
 
-        cell = Gtk.CellRendererText()
-        self.what.pack_start(cell, True)
-        self.what.add_attribute(cell, "text", 0)
+        self.type_combo = Gtk.ComboBox.new_with_model(types)
+        renderer_text = Gtk.CellRendererText()
+        self.type_combo.pack_start(renderer_text, True)
+        self.type_combo.add_attribute(renderer_text, "text", 0)
+        self.type_combo.set_active(0)
 
-        self.info.set_text("YOUR KERNEL\nRelease: {}\nMachine: {}".format(uname.release, self.arch))
-        #for x in dir(self.what): print(x)
+        hbox.pack_start(info_label, True, False, 0)
+        hbox.pack_start(self.type_combo, True, True, 0)
+        vbox.pack_start(hbox, False, False, 0)
+
+        self.listBox = Gtk.ListBox()
+        self.listBox.set_activate_on_single_click(True)
         for version in self.linux_versions:
             row = Gtk.ListBoxRow()
             row.add(Gtk.Label(version))
+            self.listBox.add(row)
 
-            self.list.add(row)
+        scrolledWindow = Gtk.ScrolledWindow()
+        scrolledWindow.add_with_viewport(self.listBox)
+        vbox.pack_start(scrolledWindow, True, True, 0)
 
-        self.window.show_all()
+        install_button = Gtk.Button(label="INSTALL")
+        install_button.connect("clicked", self.install)
+        vbox.pack_start(install_button, False, False, 0)
 
-        builder.connect_signals({"DeleteWindow":self.DeleteWindow, "install":self.install})
-
-    def DeleteWindow(self, *args):
-        Gtk.main_quit(*args)
+        self.add(vbox)
+        self.show_all()
 
     def install(self, button):
-        m = self.list.get_selected_row()
+        m = self.listBox.get_selected_row()
         version = self.linux_versions[m.get_index()]
 
         what = 'generic'
-        if self.what.get_active() != 0:
+        if self.type_combo.get_active() != 0:
             what = 'lowlatency'
 
         url = self._url + version
         Popen(['./terminal_intro'] + get_filename(url, what, self.arch))
 
 if __name__ == "__main__":
-	app = app()
-	Gtk.main()
+    app = app()
+    Gtk.main()
